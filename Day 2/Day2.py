c03078d6612de384fc6e57875c1a6fade8802cb9
BurritoFreak <chris.sarmousakis@gmail.com>
2024-12-05 23:20:22 -0500

    Day 2

--- Day 2/Day2.py
+++ Day 2/Day2.py
@@ -9,13 +9,39 @@ number_lists = read_numbers_from_file('Day 2\input.txt')
 
 num_safe_reports = 0
 
-for number_list in number_lists:
-    issafe = True
+def safe_report_tester(number_list):
     for i in range(len(number_list)-1):
         safechecker = abs(number_list[i] - number_list[i+1])
         if safechecker > 3 or safechecker < 1:
-            issafe = False
+            return False
             break
+    for i in range(len(number_list)-1):
+        if number_list[i] > number_list[i+1]:
+            inorder = True
+        else:
+            inorder = False
+            break
+    if not(inorder):
+        for i in range(len(number_list)-1):
+            if number_list[i] < number_list[i+1]:
+                inorder = True
+            else:
+                inorder = False
+                break
+    if not(inorder):
+        return False
+    return True
+
+for number_list in number_lists:
+    issafe = safe_report_tester(number_list)
+    if not(issafe):
+        for i in range(len(number_list)):
+            templist = number_list.copy()
+            templist.pop(i)
+            print(templist)
+            issafe = safe_report_tester(templist)
+            if issafe:
+                break
     if issafe:
         num_safe_reports += 1
 
